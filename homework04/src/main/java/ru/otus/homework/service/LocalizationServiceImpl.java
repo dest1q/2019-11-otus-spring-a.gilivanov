5fa5fadb9d9ebd9de5478cc18f3a16292be193cf
a.gilivanov <dest1q@icloud.com>
2020-01-13 00:22:39 +0300

    ДЗ-04

---
+++ homework04/src/main/java/ru/otus/homework/service/LocalizationServiceImpl.java
@@ -0,0 +1,25 @@
+package ru.otus.homework.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Service;
+import ru.otus.homework.config.LocalizationConfig;
+
+import java.util.Locale;
+
+@Service
+public class LocalizationServiceImpl implements LocalizationService {
+    private final MessageSource messageSource;
+    private final Locale locale;
+
+    @Autowired
+    public LocalizationServiceImpl(MessageSource messageSource, LocalizationConfig localizationConfig) {
+        this.messageSource = messageSource;
+        this.locale = Locale.forLanguageTag(localizationConfig.getTag());
+    }
+
+    @Override
+    public String getMessage(String message, String... params) {
+        return this.messageSource.getMessage(message, params, locale);
+    }
+}
