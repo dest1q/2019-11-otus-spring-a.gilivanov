6dc6f6bf66fdfa214120abe5524e86f4ec689c48
a.gilivanov <dest1q@icloud.com>
2020-03-01 22:12:51 +0300

    Remove nonreviewed code

--- homework07/src/main/java/ru/otus/homework07/exceptions/HomeworkException.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.homework07.exceptions;
-
-public class HomeworkException extends Exception{
-    HomeworkException(String message){
-        super(message);
-    }
-}
--- homework07/src/main/java/ru/otus/homework07/exceptions/NoDataFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.homework07.exceptions;
-
-public class NoDataFoundException extends HomeworkException{
-    public NoDataFoundException(String message) {
-        super(message);
-    }
-}
